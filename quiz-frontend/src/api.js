230ccf63ba555dd488a3e875b2dc060d959eee78
anjali123d <anjalidhapa22@gmail.com>
2026-02-28 11:26:34 +0530

    api url changed

--- quiz-frontend/src/api.js
+++ quiz-frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api"
+    baseURL: "https://quiz-portal-vmue.onrender.com/api"
 });
 
 // 🔐 Add interceptor to send token automatically
