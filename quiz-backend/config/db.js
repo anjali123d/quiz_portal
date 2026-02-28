7ecedb327aeb2d2b39c2458360d1824fb6b9a583
anjali123d <anjalidhapa22@gmail.com>
2026-02-28 11:43:21 +0530

    connection error solve

--- quiz-backend/config/db.js
+++ quiz-backend/config/db.js
@@ -1,4 +1,4 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
 // const connectDB = async () => {
 //     await mongoose.connect("mongodb+srv://cosmosbvn_db_user:cosmos123@mcqtestcluster0.8lbkhd3.mongodb.net/?appName=mcqTestCluster0");
